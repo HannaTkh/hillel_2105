89287e23510ec541e13be9cba2df8c41c2d4a9bd
HannaTkh <anaskarada@gmail.com>
2024-05-27 15:27:57 +0300

    f string test

--- tests2/test_push.py
+++ tests2/test_push.py
@@ -3,3 +3,10 @@
 #test bla
 
 #bla
+
+apple = 4
+pear = apple + 5
+plum = apple - 2
+together = apple + pear + plum
+
+print(f' together:\n {apple} apple + {plum} plum + {pear} pear  = {together} trees')
