739c1ec21a4d68c8e61e1b613053f558f4b19aac
HannaTkh <anaskarada@gmail.com>
2024-05-27 13:15:33 +0300

    some commit3

--- lesson2_tests/homework2_test.py
+++ lesson2_tests/homework2_test.py
@@ -1,7 +1,82 @@
-#first test 1
+# task 01 == Виправте синтаксичні помилки
+print("Hello", "world!")
 
-#refactoring
+# task 02 == Виправте синтаксичні помилки
+hello = "Hello"
+world = "world"
+if True:
+    print(f"{hello} {world}!")
 
-#test3
+# task 03  == Вcтавте пропущену змінну у ф-цію print
+for letter in "Hello world!":
+    print(letter)
 
-#test4
+# task 04 == Зробіть так, щоб кількість бананів була
+# завжди в чотири рази більша, ніж яблук
+apples = 2
+banana = apples * 4
+print(banana)
+
+# task 05 == виправте назви змінних
+storona_1 = 1
+storona_2 = 2
+storona_3 = 3
+storona_4 = 4
+
+# task 06 == Порахуйте периметр фігури з task 05
+# та виведіть його для користувача
+perimetery = storona_1 + storona_2 + storona_3 + storona_4
+print(perimetery)
+
+# task 07
+"""
+У саду посадили 4 яблуні. Груш на 5 більше яблунь, а слив - на 2 менше.
+Скільки всього дерев посадили в саду?
+"""
+
+apples = 4
+pear = apples + 5
+plum = apples - 2
+
+trees = apples + pear + plum
+print("trees " + "=" + " " + str(trees))
+
+print(trees)
+
+# task 08
+"""
+До обіда температура повітря була на 5 градусів вище нуля.
+Після обіду температура опустилася на 10 градусів.
+Надвечір потепліло на 4 градуси. Яка температура надвечір?
+"""
+
+
+morning_temp = 5
+afternoon_temp = morning_temp - 10
+evening_temp = afternoon_temp + 4
+print(evening_temp)
+
+# task 09
+"""
+Взагалі у театральному гуртку - 24 хлопчики, а дівчаток - вдвічі менше.
+1 хлопчик захворів та 2 дівчинки не прийшли сьогодні.
+Скількі сьогодні дітей у театральному гуртку?
+"""
+boys = 24
+girls = boys // 2
+today = boys - 1 + girls - 2
+print(today)
+
+# task 10
+"""
+Перша книжка коштує 8 грн., друга - на 2 грн. дороже,
+а третя - як половина вартості першої та другої разом.
+Скільки будуть коштувати усі книги, якщо купити по одному примірнику?
+"""
+book1 = 8
+book2 = book1 + 2
+book3 = (book1 + book2) // 2
+all_books = book1 + book2 + book3
+print(all_books)
+
+# test changes
