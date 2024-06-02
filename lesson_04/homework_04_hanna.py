5f17739d74445f9e67545292c3aacc879711f2ab
HannaTkh <anaskarada@gmail.com>
2024-06-02 23:33:15 +0300

    Updating homework file

--- lesson_04/homework_04_hanna.py
+++ lesson_04/homework_04_hanna.py
@@ -19,21 +19,23 @@ hour after hour. And when the middle of the afternoon came, from being a
 poor poverty, stricken boy in the .... morning, Tom was literally
 rolling in wealth."""
 
-
-
 ##  ПЕРЕЗАПИСУЙТЕ зміст змінної adwentures_of_tom_sawer у завданнях 1-3
 # task 01 ==
 """ Дані у строці adwentures_of_tom_sawer розбиті випадковим чином, через помилку.
 треба замінити кінець абзацу на пробіл .replace("\n", " ")"""
+
 separator = '- ' * 38
 print(separator)
+
 print("Task 01:")
 
 adwentures_of_tom_sawer = adwentures_of_tom_sawer.replace('\n', ' ')
 print(adwentures_of_tom_sawer)
 
 print(separator)
+
 # task 02 ==
+
 print("Task 02:")
 
 """ Замініть .... на пробіл
@@ -58,7 +60,7 @@ print(separator)
 """
 print("Task 04:")
 
-print('The letter "h" occurs in the text' , adwentures_of_tom_sawer.count('h') , "times")
+print('The letter "h" occurs in the text' , adwentures_of_tom_sawer.count('h'), "times")
 
 print(separator)
 
@@ -67,8 +69,6 @@ print(separator)
 """
 print("Task 05:")
 
-adwentures_of_tom_sawer = adwentures_of_tom_sawer.replace('"' , ' ')
-without_quotes = adwentures_of_tom_sawer.split()
 count = 0
 for big_letter in adwentures_of_tom_sawer:
     if big_letter.isupper():
@@ -98,22 +98,38 @@ print("Task 07:")
 adwentures_of_tom_sawer_sentences = adwentures_of_tom_sawer.split('. ')
 print(adwentures_of_tom_sawer_sentences)
 
-adwentures_of_tom_sawer_sentences = adwentures_of_tom_sawer.split('.')
-
+print(separator)
 
 # task 08
 """ Виведіть четверте речення з adwentures_of_tom_sawer_sentences.
 Перетворіть рядок у нижній регістр.
 """
 
-# print(adwentures_of_tom_sawer_sentences[3])
+print("Task 08:")
 
+lower_04_sentence = adwentures_of_tom_sawer_sentences[3]
+print(lower_04_sentence.lower())
 
+print(separator)
 
 # task 09
 """ Перевірте чи починається якесь речення з "By the time".
 """
-#
-# for bunggg in adwentures_of_tom_sawer.split('. '):
-#     if bunggg.startswith(' By the time'):
-#         print(bunggg)
+print("Task 09:")
+
+for begin_sentence in adwentures_of_tom_sawer.split('.'):
+    if begin_sentence.startswith(' By the time'):
+        print('There is a sentence in the text that begins with "By the time"')
+
+print(separator)
+
+# task 10
+""" Виведіть кількість слів останнього речення з adwentures_of_tom_sawer_sentences.
+"""
+print("Task 10:")
+
+last_sentence = adwentures_of_tom_sawer_sentences[-1]
+word_count = len(last_sentence.split())
+print('The number of words in the last sentence:', word_count)
+
+print(separator)
